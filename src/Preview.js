327f39d1905ae507a9a3511bbec0341f2bc44249
code-vine <caldwell.vincent@outlook.com>
2023-03-31 15:13:29 -0500

    adds Preview.js and Preview.css

---
+++ src/Preview.js
@@ -0,0 +1,11 @@
+import './Preview.css';
+import Toolbar from './Toolbar';
+
+
+function Preview(props){
+    return (
+        <div className='preview-wrap'>
+            <Toolbar title="Preview" />
+        </div>
+    )
+}
